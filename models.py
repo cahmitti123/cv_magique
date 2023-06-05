b346717f7ded2f686b77810f4b67b35097cb87fa
elmaaroufi1 <abdessamad.maaroufi12@gmail.com>
2023-06-05 15:36:55 +0100

    add check attibutes

--- models.py
+++ models.py
@@ -27,6 +27,11 @@ class Cv(Base):
     languages = Column(Text(600))
     skills = Column(Text(600))
     loisirs = Column(Text(600))
+    is_experience = Column(Boolean, default=True)
+    is_education = Column(Boolean, default=True)
+    is_languages = Column(Boolean, default=True)
+    is_skills = Column(Boolean, default=True)
+    is_loisirs = Column(Boolean, default=True)
     user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
     user = relationship("User", back_populates="cvs")
 
